1b43e7aa670560f31f2fa6d5545e4e68c3e69aae
Albert Kim <albertk@princeton.edu>
2022-03-06 22:58:32 -0500

    added camera buttons

--- Scripts/Unit_Selection_Manager.gd
+++ Scripts/Unit_Selection_Manager.gd
@@ -28,7 +28,7 @@ func _process(_delta):
 	if not is_instance_valid(hovered):
 		hovered = null
 	
-	mouse_pos = get_global_mouse_position()
+	mouse_pos = get_node("/root/Testing_Area").get_global_mouse_position()
 	
 	var space_state = get_world_2d().get_direct_space_state()
 	var result = space_state.intersect_point(mouse_pos, 1, [], 1)
@@ -66,7 +66,6 @@ func _process(_delta):
 				unit_UI.set_initial_values(null, selected.attack_damage, selected.defense, 
 				selected.attack_speed, selected.movement_speed, selected.max_hp, selected.max_mana)
 				unit_UI.visible = true
-				print("apple clicked")
 		
 	#-------------------------------------------
 	# Hold + move
@@ -77,7 +76,7 @@ func _process(_delta):
 				unit_starting_pos = selected.global_position
 				
 	if Input.is_action_just_released("left_click"):
-		
+		print(mouse_pos)
 		if selected != null:
 			if selected.is_colliding():
 				selected.global_position = unit_starting_pos
